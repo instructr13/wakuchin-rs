125abce45a9ad1cf08217679f5b706dcae1eec12
Potato1682 <ptt1682@gmail.com>
2022-07-17 00:51:44 +0900

    chore: for_each to for statement

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -30,13 +30,15 @@ mod test {
     let mut wakuchin_c_count: u32 = 0;
     let mut wakuchin_n_count: u32 = 0;
 
-    wakuchin.chars().for_each(|c| match c {
-      symbol::WAKUCHIN_W => wakuchin_w_count += 1,
-      symbol::WAKUCHIN_K => wakuchin_k_count += 1,
-      symbol::WAKUCHIN_C => wakuchin_c_count += 1,
-      symbol::WAKUCHIN_N => wakuchin_n_count += 1,
-      _ => panic!("Unexpected char: {}", c),
-    });
+    for c in wakuchin.chars() {
+      match c {
+        symbol::WAKUCHIN_W => wakuchin_w_count += 1,
+        symbol::WAKUCHIN_K => wakuchin_k_count += 1,
+        symbol::WAKUCHIN_C => wakuchin_c_count += 1,
+        symbol::WAKUCHIN_N => wakuchin_n_count += 1,
+        _ => panic!("Unexpected character: {}", c),
+      }
+    }
 
     assert_eq!(wakuchin_w_count, 3);
     assert_eq!(wakuchin_k_count, 3);
