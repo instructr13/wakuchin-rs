55e241a712039132505712c6c7ddeba8aac0f870
Potato1682 <ptt1682@gmail.com>
2022-07-17 23:23:58 +0900

    test: fix arguments passed by benchmarks

--- core/benches/benchmarks/worker.rs
+++ core/benches/benchmarks/worker.rs
@@ -7,12 +7,13 @@ use wakuchin_core::worker::run_seq;
 
 fn speed_par(c: &mut Criterion) {
   c.bench_function("parallel processing speed", |b| {
-    b.iter(|| run_par(30000, 2, Regex::new(r"^WKNCWKNC$").unwrap()))
+    b.iter(|| run_par(30000, 2, Regex::new(r"^WKNCWKNC$").unwrap(), |_| {}));
   });
 }
+
 fn speed_seq(c: &mut Criterion) {
   c.bench_function("sequential processing speed", |b| {
-    b.iter(|| run_seq(30000, 2, Regex::new(r"^WKNCWKNC$").unwrap()))
+    b.iter(|| run_seq(30000, 2, Regex::new(r"^WKNCWKNC$").unwrap(), |_| {}));
   });
 }
 
