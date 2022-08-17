6e04b05a8dbc6a08b149e3124dda3f0576f33a85
Potato1682 <ptt1682@gmail.com>
2022-08-18 06:12:14 +0900

    refactor(cli): use fs::read_to_string instead of assining to mut string

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -2,18 +2,14 @@ use std::error::Error;
 use std::path::Path;
 
 use anyhow::anyhow;
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
+use tokio::fs::read_to_string;
 
 use crate::app::Config;
 
 pub(crate) async fn load_config(path: &Path) -> Result<Config, Box<dyn Error>> {
-  let mut file = File::open(path)
+  let contents = read_to_string(path)
     .await
     .map_err(|e| format!("'{}': {}", path.to_string_lossy(), e))?;
-  let mut contents = String::new();
-
-  file.read_to_string(&mut contents).await?;
 
   let config: Config = match path.extension().unwrap().to_str().unwrap() {
     "json" => serde_json::from_str(&contents)?,
