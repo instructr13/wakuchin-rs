bd57e20ddc8170ca1d83422f6b16c530490332fa
Potato1682 <ptt1682@gmail.com>
2022-10-14 13:23:53 +0000

    feat: pretty print config errors

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -2,6 +2,7 @@ use std::borrow::Borrow;
 use std::path::Path;
 
 use anyhow::{anyhow, Result};
+use format_serde_error::SerdeError;
 use tokio::fs::read_to_string;
 
 use crate::app::Config;
@@ -19,9 +20,13 @@ pub(crate) async fn load_config(path: &Path) -> Result<Config> {
     .to_string_lossy()
     .borrow()
   {
-    "json" => serde_json::from_str(&contents)?,
-    "yaml" | "yml" => serde_yaml::from_str(&contents)?,
-    "toml" => toml::from_str(&contents)?,
+    "json" => serde_json::from_str(&contents)
+      .map_err(|e| SerdeError::new(contents, e))?,
+    "yaml" | "yml" => serde_yaml::from_str(&contents)
+      .map_err(|e| SerdeError::new(contents, e))?,
+    "toml" => {
+      toml::from_str(&contents).map_err(|e| SerdeError::new(contents, e))?
+    }
     _ => Err(anyhow!("'{}': Invalid config type", path.to_string_lossy()))?,
   };
 
