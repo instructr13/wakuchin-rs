246f60edb2e9f3d419a477a4817b7c9d42bd0d5f
Potato1682 <self@potato1682.work>
2023-01-14 06:36:11 +0900

    chore(core): more benchmarks!

--- core/benches/benchmarks/worker.rs
+++ core/benches/benchmarks/worker.rs
@@ -1,3 +1,4 @@
+use std::thread::available_parallelism;
 use std::time::Duration;
 
 use criterion::{criterion_group, Criterion};
@@ -9,12 +10,32 @@ use wakuchin::worker::run_par;
 use wakuchin::worker::run_seq;
 
 fn speed_par(c: &mut Criterion) {
-  c.bench_function("parallel processing speed", |b| {
+  let regex = Regex::new(r"^WKNCWKNC$").unwrap();
+
+  c.bench_function("parallel processing speed with two workers", |b| {
     b.iter(|| {
       run_par(
-        30000,
+        20000,
         2,
-        &Regex::new(r"^WKNCWKNC$").unwrap(),
+        &regex,
+        Box::new(EmptyProgressHandler::new()),
+        Duration::from_millis(20),
+        2,
+      )
+    });
+  });
+
+  println!(
+    "INFO: Depends on your machine spec; actual workers is {}",
+    available_parallelism().unwrap()
+  );
+
+  c.bench_function("parallel processing speed with maximum workers", |b| {
+    b.iter(|| {
+      run_par(
+        20000,
+        2,
+        &regex,
         Box::new(EmptyProgressHandler::new()),
         Duration::from_millis(20),
         0,
@@ -27,7 +48,7 @@ fn speed_seq(c: &mut Criterion) {
   c.bench_function("sequential processing speed", |b| {
     b.iter(|| {
       run_seq(
-        30000,
+        20000,
         2,
         &Regex::new(r"^WKNCWKNC$").unwrap(),
         Box::new(EmptyProgressHandler::new()),
